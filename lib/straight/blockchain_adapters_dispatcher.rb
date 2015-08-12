0fb54dff81b24fd414c70ebe8625e91011b4d037
Dmitry Tymchuk <dsnipe@gmail.com>
2015-08-12 19:02:12 +0300

    Naive implementation for parallel using many adapters

---
+++ lib/straight/blockchain_adapters_dispatcher.rb
@@ -0,0 +1,58 @@
+module Straight
+  class BlockchainAdaptersDispatcher
+    # It calls to many adapters simultaneously. How much adapters determines in STEP constant.
+    # If all adapters fails it raise last exception.
+
+    STEP = 2
+    attr_reader :list_position, :adapters, :result, :step
+
+    def initialize(adapters, &block)
+      @list_position = 0
+      @result = nil
+      @step = 0
+      @adapters = adapters
+      run_requests(block) if block_given?
+    end
+
+    def get_adapters
+      @step = [adapters.size - @list_position, STEP].min
+      adapters = @adapters[@list_position..@step-1]
+      @list_position += @step
+      adapters
+    end
+
+    def run_requests(block)
+      threads = []
+      fault_counter = 0
+      get_adapters.each do |adapter| 
+        t = Thread.new do
+          block.call(adapter)
+        end
+        t.abort_on_exception = true
+        threads << t
+      end
+      wait_values(threads)
+    rescue => e
+      raise e if fault_counter == @step && @list_position == @adapters.size
+      if fault_counter == @step 
+        fault_counter = 0
+        retry
+      end
+      fault_counter += 1
+    end
+
+    def wait_values(threads)
+      threads.each do |t| 
+        Thread.new do
+          if val = t.value
+            @result = val
+            threads.map(&:kill)
+          end
+        end
+      end
+      loop { break if @result }
+      @result
+    end
+
+  end
+end
